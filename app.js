d4c6626cd92d09cf6f042499b542b384d7f2af25
Rupaprasanna <rupaambati9@gmail.com>
2023-06-30 14:39:07 +0000

    expressgadgetspage

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/gadgets", (request, response) => {
-  response.sendFile("./gadgets.html");
+  response.sendFile("./gadgets.html", { root: __dirname });
 });
 module.exports = app;
+app.listen(3003);
